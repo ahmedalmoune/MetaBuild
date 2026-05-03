48087f5295c39df9ace190d48cadd578e0280b75
Ahmed A <163431268+ahmedalmoune@users.noreply.github.com>
2026-05-03 00:30:22 -0400

    Update lint/TS settings

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+/** @type {import("eslint").Linter.Config[]} */
+const config = [
+  ...nextVitals,
+];
+
+export default config;
