36893c314dbff44a7f418203599db96496af29e0
Ahmed A <163431268+ahmedalmoune@users.noreply.github.com>
2026-05-13 00:30:29 -0400

    A

--- frontend/src/components/build-form/country-dropdown.tsx
+++ frontend/src/components/build-form/country-dropdown.tsx
@@ -6,24 +6,55 @@
 * TODO: Finish this
 */
 
-import { COUNTRIES, FORM_FIELDS } from "@/constants/build-preferences";
+import { COUNTRIES, DEFAULT_COUNTRY, FORM_FIELDS } from "@/constants/build-preferences";
+import { ERROR_MESSAGES } from "@/constants/general";
 import styles from "@/styles/page.module.css";
 import { useBuildQueryState } from "@/utils/build-query";
-
+import * as Flags from "country-flag-icons/react/3x2";
+import { useCallback, useEffect } from "react";
+import { toast } from "sonner";
 
 export default function CountryDropdown() {
   const [queryState, setQueryState] = useBuildQueryState();
-  // const selectedCountry = COUNTRIES.find(c => c.code === queryState[FORM_FIELDS.country]);
-  
+
+
+  const validateCountry = useCallback(() => {
+    return COUNTRIES.some(country => country.code === queryState[FORM_FIELDS.country]);
+  }, [queryState]);
+
+
+
+ // const CountryFlag = Flags[queryState[FORM_FIELDS.country] as keyof typeof Flags] || Flags.US;
+
+  useEffect(() => {
+    try {
+      // Validate country on mount
+      if (!validateCountry()) {
+        setQueryState({ [FORM_FIELDS.country]: DEFAULT_COUNTRY.code });
+      }
+    
+    } catch (error) {
+      toast.error(ERROR_MESSAGES.queryValue);
+    }
+  }, [setQueryState, validateCountry]);
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    try {
+      setQueryState({ [FORM_FIELDS.country]: e.target.value });
+    } catch (error) {
+      toast.error(ERROR_MESSAGES.queryValue);
+    }
+  };
+
   return (
     <div className="mb-4 mt-2 align-self-end">
-      <label htmlFor="country" className="fs-6 fw-semibold">
-        {queryState[FORM_FIELDS.country]}
-      </label>
-      <select name="country" id="country" className={`form-select form-select-sm w-auto d-inline-block shadow-sm 
-        ${styles.lightScrollbar}`} onChange={(e) => setQueryState({ [FORM_FIELDS.country]: e.target.value })} 
-        value={queryState[FORM_FIELDS.country]}
-      >
+      <div className={`me-2 border border-secondary`} style={{ width: "2rem" }}>
+        {Flags[queryState[FORM_FIELDS.country] as keyof typeof Flags]}
+      </div>
+      <select className={`form-select form-select-sm w-auto d-inline-block shadow-sm ${styles.lightScrollbar}`} 
+        name={FORM_FIELDS.country} value={queryState[FORM_FIELDS.country]} onChange={handleChange}>
+          
         {COUNTRIES.map((country) => (
           <option key={country.code} value={country.code} disabled={country.code === queryState[FORM_FIELDS.country]}>
             {country.value}
